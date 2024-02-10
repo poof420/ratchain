78e79aad326a02bea6bc550a7db60d48f55fb54a
poof420 <91919121+poof420@users.noreply.github.com>
2024-02-10 14:45:15 -0600

    Add files via upload

--- script working.js
+++ script working.js
@@ -1,5 +1,5 @@
 // Example adjustments in script working.js
-import * as THREE from '/three/build/three.js';
+import * as THREE from './three/three.module.js';
 import { MTLLoader } from '/three/examples/jsm/loaders/MTLLoader.js';
 import { OBJLoader } from '/three/examples/jsm/loaders/OBJLoader.js';
 
